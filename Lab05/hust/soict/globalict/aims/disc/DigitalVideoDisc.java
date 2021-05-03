0776e79dfd8822edf143b2c1eebb179a688d78db
Jason3110 <80505552+Jason3110@users.noreply.github.com>
2021-05-03 21:28:33 +0700

    Add files via upload

---
+++ Lab05/hust/soict/globalict/aims/disc/DigitalVideoDisc.java
@@ -0,0 +1,77 @@
+package hust.soict.globalict.aims.disc;
+
+import java.util.Locale;
+
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setDirectory(String director) {
+        this.director = director;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, float cost) {
+        super();
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        super();
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+
+    public boolean Search(String title) {
+        String[] str = title.split(" ");
+        for (String a : str) {
+            if (!this.getTitle().toLowerCase().contains(a.toLowerCase())) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Lab05/hust/soict/globalict/test/utils/TestDate.java
@@ -0,0 +1,28 @@
+package hust.soict.globalict.test.utils;
+import hust.soict.globalict.aims.utils.*;
+
+public class TestDate {
+    public static void main(String[] args) {
+        MyDate date1, date2, date3, date4;
+        date1 = new MyDate(16,12,2001);
+        date2 = new MyDate(31,10,2012);
+        date3 = new MyDate(16,2,2002);
+        date4 = new MyDate(26,11,2020);
+        System.out.println("Print test:");
+        date4.print();
+        MyDate.chooseformat(1,date1);
+        System.out.println("\n" + "Compare test: ");
+        if(DateUtil.compare(date2,date3)==1) {
+            MyDate.chooseformat(2,date3);
+            System.out.print(" is before ");
+            MyDate.chooseformat(2,date3);
+        }
+        else {
+            MyDate.chooseformat(3,date3);
+            System.out.print(" is before ");
+            MyDate.chooseformat(3,date4);
+        }
+        System.out.println("\n"+"Sorting test: ");
+        DateUtil.sort(date1,date2,date3,date4);
+    }
+}
